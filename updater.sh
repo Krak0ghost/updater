f8a9864f8e91609d0584a691bebb916700ecf397
Krak <xyz@example.com>
2019-02-24 16:17:13 +0100

    add repository of docker

--- updater.sh
+++ updater.sh
@@ -39,6 +39,11 @@ function install_docker() {
 
     sudo apt-key fingerprint 0EBFCD88
 
+    sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
 }
 
 
