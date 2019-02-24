09cd5d0aaf795ae8e6be4ef5f054b7063f4d126d
Krak <xyz@example.com>
2019-02-24 16:16:33 +0100

    add fingerprint on docker_install

--- updater.sh
+++ updater.sh
@@ -34,8 +34,10 @@ function install_docker() {
     curl \
     gnupg-agent \
     software-properties-common
-    
+
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+    sudo apt-key fingerprint 0EBFCD88
 
 }
 
