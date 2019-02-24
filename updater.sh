c72394760737cabec14bc5b473375df413198540
Krak <xyz@example.com>
2019-02-24 16:10:06 +0100

    add function install install_msf and add installer metasploit

--- updater.sh
+++ updater.sh
@@ -16,6 +16,13 @@ function install() {
 
 }
 
+function install_msf() {
+
+  curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+  chmod 755 msfinstall && \
+./msfinstall
+
+}
 
 
 function main() {
