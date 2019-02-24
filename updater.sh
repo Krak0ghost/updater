9ce5b1d169804d4e5e6f6c824725b4dbeb06710f
Krak <xyz@example.com>
2019-02-24 16:07:18 +0100

    add install nmap

--- updater.sh
+++ updater.sh
@@ -10,7 +10,10 @@ function install() {
 
     sudo apt install leafpad -y
     sudo apt install net-tools -y
-    apt install keepass2 -y
+    sudo apt install keepass2 -y
+    sudo apt install nmap -y
+
+
 }
 
 
